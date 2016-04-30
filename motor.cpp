7ea2b288c6af47a51b59d4db4b949ab12ca17a64
Darshan Rai <darshanjrai@gmail.com>
2016-04-30 16:53:21 +0530

    Correctly indent motor.cpp

--- motor.cpp
+++ motor.cpp
@@ -2,23 +2,23 @@
 
 Motor::Motor(BlackLib::pwmName pin)
 {
-    motor = new BlackLib::BlackPWM(pin);
-    /* if new period value is less than the current duty value,
-     * the new period value setting operation won't execute. So
-     * firstly duty value is set to zero for safe steps. Duty can
-     * be set to 0 (the value returned by
-     * BlackLib::BlackPWM::getDutyValue()). This is achieved by
-     * setDutyPercent(100)
-     */
-    motor->setDutyPercent(100);
-    motor->setPeriodTime(PWM_PERIOD, BlackLib::milisecond);
-    this->set_power(0);
+	motor = new BlackLib::BlackPWM(pin);
+	/* if new period value is less than the current duty value,
+	 * the new period value setting operation won't execute. So
+	 * firstly duty value is set to zero for safe steps. Duty can
+	 * be set to 0 (the value returned by
+	 * BlackLib::BlackPWM::getDutyValue()). This is achieved by
+	 * setDutyPercent(100)
+	 */
+	motor->setDutyPercent(100);
+	motor->setPeriodTime(PWM_PERIOD, BlackLib::milisecond);
+	this->set_power(0);
 }
 
 Motor::~Motor()
 {
-    this->set_power(0);
-    free(this);
+	this->set_power(0);
+	free(this);
 }
 
 /*
@@ -26,8 +26,8 @@ Motor::~Motor()
  */
 float Motor::get_power()
 {
-    return (motor->getNumericValue() - MIN_POWER_DUTY_PERC)
-        * MAX_MAPPED_DUTY / (MAX_POWER_DUTY_PERC - MIN_POWER_DUTY_PERC);
+	return (motor->getNumericValue() - MIN_POWER_DUTY_PERC)
+		* MAX_MAPPED_DUTY / (MAX_POWER_DUTY_PERC - MIN_POWER_DUTY_PERC);
 }
 
 /*
@@ -35,10 +35,10 @@ float Motor::get_power()
  */
 bool Motor::set_power(float power)
 {
-    if (power < 0 || power > 100)
-        return false;
+	if (power < 0 || power > 100)
+		return false;
 
-    return motor->setDutyPercent(power / MAX_MAPPED_DUTY * (MAX_POWER_DUTY_PERC
-                                                            - MIN_POWER_DUTY_PERC)
-                                 + MIN_POWER_DUTY_PERC);
+	return motor->setDutyPercent(power / MAX_MAPPED_DUTY * (MAX_POWER_DUTY_PERC
+								- MIN_POWER_DUTY_PERC)
+				     + MIN_POWER_DUTY_PERC);
 }
