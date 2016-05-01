2f856fe2b819c9de3599f34ae887e70226070398
Darshan Rai <darshanjrai@gmail.com>
2016-05-01 20:22:23 +0530

    Provide a utils file with function for elapsed time
    Instead of calling clock_gettime() and messing with the timespec
    struct, this centralized function returns the elapsed time in
    seconds with float precision.

---
+++ utils.h
@@ -0,0 +1,10 @@
+#include <time.h>
+
+float elapsed_time_in_s()
+{
+    struct timespec time_struct;
+
+    clock_gettime(CLOCK_MONOTONIC, &time_struct);
+
+    return (time_struct.tv_sec + time_struct.tv_nsec / 1.0e9);
+}
