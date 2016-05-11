adb49a43927b30f0bda42a476eed478b08441346
Darshan Rai <darshanjrai@gmail.com>
2016-05-11 20:06:43 +0530

    Update init.bash

--- init.bash
+++ init.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 set -e
+
 # enable SPIDEV
-echo ADAFRUIT-SPI0 > /sys/devices/bone_capemgr.9/slots
+
+# echo ADAFRUIT-SPI0 > /sys/devices/bone_capemgr.9/slots
+# echo BB-SPIDEV0 > /sys/devices/bone_capemgr.9/slots
+echo BB-SPI0-01 > /sys/devices/bone_capemgr.9/slots
