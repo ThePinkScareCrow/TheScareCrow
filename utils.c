5640c0c1dcc3476a8ef9f3c32d7e44ac07f3df67
Darshan Rai <darshanjrai@gmail.com>
2016-05-11 23:42:08 +0530

    Utils: Add constrain function

--- utils.c
+++ utils.c
@@ -9,3 +9,13 @@ float elapsed_time_in_s()
 
     return (time_struct.tv_sec + time_struct.tv_nsec / 1.0e9);
 }
+
+inline float constrain(float value, float low, float high)
+{
+    if (value > high)
+        return high;
+    else if (value < low)
+        return low;
+    else
+        return value;
+}
--- utils.h
+++ utils.h
@@ -2,5 +2,6 @@
 #define _UTILS_H_
 
 float elapsed_time_in_s();
+inline float constrain(float value, float low, float high);
 
 #endif
